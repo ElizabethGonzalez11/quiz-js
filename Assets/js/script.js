14ca7a6b729149f282807a21a8a12b07d8905509
Elizabeth Gonzalez <ebgonzalez@elizabeths-mbp.lan>
2023-01-05 21:04:38 -0600

    finished some CSS for questions/answers and outlined needed items for JS.

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+console.log('this is working');
+
+
+//create array
+//custom attr of data- (this goes on HTML)
+//variables: question, answers, timer that starts, score
+
+
+
+//questions need to be pulled out
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
